564a9526ca5560c36e661592f37413b59e0a6206
Robert Sebescen <robert.sebescen@devtechgroup.com>
2016-03-11 16:21:52 +0100

    Init

---
+++ Tests/StringCalculator.php
@@ -0,0 +1,12 @@
+<?php
+use Helpers\StringCalculator;
+
+/**
+ * Created by PhpStorm.
+ * User: robert.sebescen
+ * Date: 29.2.2016.
+ * Time: 2:28 PM
+ */
+class StringCalculatorTests extends PHPUnit_Framework_TestCase
+{
+}
